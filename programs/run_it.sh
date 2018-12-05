1dd90e76b80026e23b39f12089ee5576e2ed4225
c-l-nguyen <christopher811@gmail.com>
2018-12-04 23:21:09 -0600

    generated data files for men's 400m sprint for all in-scope years and corrected get_data.py to only get first table on page and remove magic numbers for total participants

---
+++ programs/run_it.sh
@@ -0,0 +1,30 @@
+# Run every possiblity and extract CSV
+
+python3 get_data.py https://www.olympic.org/athens-1896/athletics/400m-men
+python3 get_data.py https://www.olympic.org/paris-1900/athletics/400m-men
+python3 get_data.py https://www.olympic.org/st-louis-1904/athletics/400m-men
+python3 get_data.py https://www.olympic.org/london-1908/athletics/400m-men
+python3 get_data.py https://www.olympic.org/stockholm-1912/athletics/400m-men
+python3 get_data.py https://www.olympic.org/antwerp-1920/athletics/400m-men
+python3 get_data.py https://www.olympic.org/paris-1924/athletics/400m-men
+python3 get_data.py https://www.olympic.org/amsterdam-1928/athletics/400m-men
+python3 get_data.py https://www.olympic.org/los-angeles-1932/athletics/400m-men
+python3 get_data.py https://www.olympic.org/berlin-1936/athletics/400m-men
+python3 get_data.py https://www.olympic.org/london-1948/athletics/400m-men
+python3 get_data.py https://www.olympic.org/helsinki-1952/athletics/400m-men
+python3 get_data.py https://www.olympic.org/melbourne-/-stockholm-1956/athletics/400m-men
+python3 get_data.py https://www.olympic.org/rome-1960/athletics/400m-men
+python3 get_data.py https://www.olympic.org/tokyo-1964/athletics/400m-men
+python3 get_data.py https://www.olympic.org/mexico-1968/athletics/400m-men
+python3 get_data.py https://www.olympic.org/munich-1972/athletics/400m-men
+python3 get_data.py https://www.olympic.org/montreal-1976/athletics/400m-men
+python3 get_data.py https://www.olympic.org/moscow-1980/athletics/400m-men
+python3 get_data.py https://www.olympic.org/los-angeles-1984/athletics/400m-men
+python3 get_data.py https://www.olympic.org/seoul-1988/athletics/400m-men
+python3 get_data.py https://www.olympic.org/barcelona-1992/athletics/400m-men
+python3 get_data.py https://www.olympic.org/atlanta-1996/athletics/400m-men
+python3 get_data.py https://www.olympic.org/sydney-2000/athletics/400m-men
+python3 get_data.py https://www.olympic.org/athens-2004/athletics/400m-men
+python3 get_data.py https://www.olympic.org/beijing-2008/athletics/400m-men
+python3 get_data.py https://www.olympic.org/london-2012/athletics/400m-men
+python3 get_data.py https://www.olympic.org/rio-2016/athletics/400m-men
